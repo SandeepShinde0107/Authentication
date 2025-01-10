00279e44d571c065482cf60b34ea44274dee94ac
Royce-2003 <sandeepshinde0107@gmail.com>
2025-01-10 16:34:03 +0530

    second commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/userAuth', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log('Connected to MongoDB')).catch(err => console.error('MongoDB connection error:', err));
